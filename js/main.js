412aacf1bba6be57c8fe73b7dc4c485c2300c883
maria <marivasi11@yandex.ru>
2020-01-26 14:04:17 +0700

    Первая попытка создать моки

--- js/main.js
+++ js/main.js
@@ -10,13 +10,7 @@ function getRandomNumber(number) {
   return Math.floor(Math.random() * number);
 }
 
-// Функция создания DOM-элемента на основе JS-объекта
-// 1. Адрес картинки
-for (var i = 1; i <= 25; i++) {
-  var urlPicture = 'photos/' + i + '.jpg';
-}
-
-// 2. Описание фотографий
+// Создаем данные для моки
 var messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var names = ['Марс', 'Юпитер', 'Луна', 'Солнце', 'Звезда', 'Космос'];
 var comments = [
@@ -24,33 +18,9 @@ var comments = [
     avatar: 'img/avatar' + getRandomArbitrary(1, 6) + '.svg',
     message: messages[getRandomNumber(messages.length)],
     name: names[getRandomNumber(names.length)]
-}
+  }
 ];
 
-function makeElement(tagName, className) {
-  var picture = document.createElement(tagName);
-  picture.classList.add(className);
-
-  return picture;
+for (var i = 1; i <= 25; i++) {
+  var urlPicture = 'photos/' + i + '.jpg'; // как вывести все итерации, а не последнюю?
 }
-
-// Функция заполнения блока DOM-элементами на основе массива JS-объектов
-function createPicture() {
-  var listItem = makeElement('div', 'picture');
-
-  var picture = makeElement('img', 'picture__img');
-  picture.src = urlPicture;
-  picture.alt = ' ';
-  listItem.appendChild(picture);
-
-  var pictureInfo = makeElement('p', 'picture__info');
-  listItem.appendChild(pictureInfo);
-  var pictureComment = makeElement('span', 'picture__comments');
-  listItem.appendChild(pictureComment);
-  var pictureLike = makeElement('span', 'picture__likes');
-  listItem.appendChild(pictureLike);
-
-  return listItem;
-}
-
-createPicture();
