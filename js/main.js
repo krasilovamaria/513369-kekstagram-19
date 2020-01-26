a246a905ad3d027d1f77c0d49b4ff9f90768768f
maria <marivasi11@yandex.ru>
2020-01-26 18:43:40 +0700

    Создан массив из 25 сгенерированных JS объектов

--- js/main.js
+++ js/main.js
@@ -13,14 +13,30 @@ function getRandomNumber(number) {
 // Создаем данные для моки
 var messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var names = ['Марс', 'Юпитер', 'Луна', 'Солнце', 'Звезда', 'Космос'];
-var comments = [
-  {
-    avatar: 'img/avatar' + getRandomArbitrary(1, 6) + '.svg',
-    message: messages[getRandomNumber(messages.length)],
-    name: names[getRandomNumber(names.length)]
+// Функция для создания массива из 25 сгенерированных JS объектов
+function getArrayPhotos() {
+  var arr = [];
+  for (var i = 0; i < 25; i++) {
+    arr[i] = {
+      url: 'photos/' + i + '.jpg',
+      description: ' ', // не поняла что сюда записывать
+      likes: getRandomArbitrary(15, 200),
+      comments: {
+        avatar: 'img/avatar' + getRandomArbitrary(1, 6) + '.svg',
+        message: messages[getRandomNumber(messages.length)],
+        name: names[getRandomNumber(names.length)]
+      }
+    };
   }
-];
-
-for (var i = 1; i <= 25; i++) {
-  var urlPicture = 'photos/' + i + '.jpg'; // как вывести все итерации, а не последнюю?
+  return arr;
 }
+getArrayPhotos();
+
+/*
+На основе данных, созданных в предыдущем пункте и шаблона #picture создайте DOM-элементы,
+соответствующие фотографиям и заполните их данными из массива:
+Адрес изображения url подставьте как src изображения.
+Количество лайков likes подставьте как текстовое содержание элемента .picture__likes.
+Количество комментариев comments подставьте как текстовое содержание элемента .picture__comments.
+*/
+
