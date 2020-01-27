570bbf4d4f1de14424e71b406c5d0b2e5c52c7d8
maria <marivasi11@yandex.ru>
2020-01-27 21:02:57 +0700

    Исправляет замечания

--- js/main.js
+++ js/main.js
@@ -1,34 +1,55 @@
 'use strict';
+// Создает данные для моки
+var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var NAMES = ['Марс', 'Юпитер', 'Луна', 'Солнце', 'Звезда', 'Космос'];
+var QUANTITY_MIN_OBJECT = 1;
+var QUANTITY_MAX_OBJECT = 25;
+var QUANTITY_MIN_COMMENT = 1;
+var QUANTITY_MAX_COMMENT = 6;
+var QUANTITY_MIN_LIKE = 15;
+var QUANTITY_MAX_LIKE = 200;
 
-// Функция генерации случайных чисел в заданном диапазоне
+// Функция генерации случайных чисел
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-// Функция генерации случайных чисел
-function getRandomNumber(number) {
-  return Math.floor(Math.random() * number);
+// Функция, которая принимает массив в параметр и вернет его случайный элемент
+function makeRandomNumberWithArr(arr) {
+  var number = arr[getRandomArbitrary(arr.length)];
+
+  return number;
 }
 
-// Создаем данные для моки
-var messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var names = ['Марс', 'Юпитер', 'Луна', 'Солнце', 'Звезда', 'Космос'];
-// Функция для создания массива из 25 сгенерированных JS объектов
-function getArrayPhotos() {
-  var arr = [];
-  for (var i = 0; i < 25; i++) {
-    arr[i] = {
-      url: 'photos/' + i + '.jpg',
-      description: ' ', // не поняла, что сюда записывать
-      likes: getRandomArbitrary(15, 200),
-      comments: {
-        avatar: 'img/avatar' + getRandomArbitrary(1, 6) + '.svg',
-        message: messages[getRandomNumber(messages.length)],
-        name: names[getRandomNumber(names.length)]
-      }
+// Функция для создания комментариев
+function getComments() {
+  var comments = [];
+
+  for (var i = QUANTITY_MIN_OBJECT; i < QUANTITY_MAX_OBJECT; i++) {
+    comments[i] = {
+      avatar: 'img/avatar' + getRandomArbitrary(QUANTITY_MIN_COMMENT, QUANTITY_MAX_COMMENT) + '.svg',
+      message: makeRandomNumberWithArr(MESSAGES),
+      name: makeRandomNumberWithArr(NAMES)
     };
   }
-  return arr;
+
+  return comments;
+}
+
+// Функция для создания массива из QUANTITY_OBJECT сгенерированных JS объектов
+function getArrayPhotos() {
+  var photos = [];
+
+  for (var i = QUANTITY_MIN_OBJECT; i < QUANTITY_MAX_OBJECT; i++) {
+    photos[i] = {
+      url: 'photos/' + i + '.jpg',
+      description: 'description',
+      likes: getRandomArbitrary(QUANTITY_MIN_LIKE, QUANTITY_MAX_LIKE),
+      comments: getComments()
+    };
+  }
+
+  return photos;
 }
 
 // Шаблон template в документе
@@ -36,9 +57,9 @@ var templatePicture = document.querySelector('#picture').content;
 
 // Функция, которая клониурет узел(template) и на его основе создает DOM-элементы
 function getPhotosInDom() {
-  var objPhotos = getArrayPhotos(); // и как мне их использовать?????
+  var objPhotos = getArrayPhotos();
 
-  for (var i = 0; i < 25; i++) {
+  for (var i = QUANTITY_MIN_OBJECT; i < QUANTITY_MAX_OBJECT; i++) {
     // Копирует template
     var photoItems = templatePicture.cloneNode(true);
     // Находит элементы, которые нужно заполнить
@@ -46,11 +67,12 @@ function getPhotosInDom() {
     var pictureComments = photoItems.querySelector('.picture__comments');
     var pictureLikes = photoItems.querySelector('.picture__likes');
     // Заполняет шаблон
-    pictureImg.src = 'photos/' + i + '.jpg';
-    pictureLikes.textContent = getRandomArbitrary(15, 200);
-    pictureComments.textContent = getRandomArbitrary(1, 6);
+    pictureImg.src = objPhotos[i].url;
+    pictureLikes.textContent = objPhotos[i].likes;
+    pictureComments.textContent = objPhotos[i].comments;
   }
 
   return photoItems;
 }
 
+getPhotosInDom();
