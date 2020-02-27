912c3edb2a3d1b8f8292a6e7c7b6971d85e36119
maria <marivasi11@yandex.ru>
2020-02-27 15:10:02 +0700

    Попытка исправить обработчики в form.js и фильтрафцию в filtering.js

--- js/filtering.js
+++ js/filtering.js
@@ -1,12 +1,9 @@
 'use strict';
 // модуль, который добавляет фильтрацию изображений
 (function () {
-  // 10 случайных фотографий
   var RANDOM_PHOTO = 10;
 
   var filterButton = document.querySelector('.img-filters__button');
-  var filterButtonActive = document.querySelector('.img-filters__button--active');
-  // ссылка на элемент, которая обновляется при смене активного фильтра
   var filterActive;
 
   // обработчик изменения фильтров
@@ -14,24 +11,36 @@
     // находит фильтр на котором произошел клик
     var target = evt.target;
 
-    // убирает все фотографии отрисованные ранее
-    window.element.pictures.innerHTML = '';
-
     // если это не фильтр на котором произошел клик
-    if (!target.classList.containes('.img-filters__button--active')) {
+    if (!target.classList.containes('img-filters__button--active')) {
+      filterActive = document.querySelector('.img-filters__button--active');
       // то удаляет, что это активный фильтр
       filterActive.classList.remove('img-filters__button--active');
       // и добавляет активный фильтр на котором произошел клик
       target.classList.add('img-filters__button--active');
+      // ссылка на элемент, которая обновляется при смене активного фильтра
+      filterActive = target;
+
+      // убирает все фотографии отрисованные ранее
+      window.element.pictures.innerHTML = '';
+
       // создает копию массива
       var dataCopy = window.element.data.slice();
+
+      if (target.textContent === 'Случайные') {
+        // выводит 10 случайных, не повторяющихся фотографий
+        dataCopy = dataCopy.slice(0, RANDOM_PHOTO); // КАК СДЕЛАТЬ НЕ ПОВТОРЯЮЩИЕСЯ ФОТО???
+      } else if (target.textContent === 'Обсуждаемые') {
+        // фотографии, отсортированные в порядке убывания количества комментариев
+        dataCopy = dataCopy.slice().sort(function (a, b) {
+          return b.comments.length - a.comments.length;
+        });
+      }
+
+      window.element.onSuccess(dataCopy);
     }
   };
 
-  // По умолчанию — фотографии в изначальном порядке с сервера
-  // Случайные — 10 случайных, не повторяющихся фотографий
-  // Обсуждаемые — фотографии, отсортированные в порядке убывания количества комментариев
-
-
+  filterButton.addEventListener('click', onFilterClick);
 })();
 
